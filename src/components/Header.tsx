7ad7d9a72272031a0ee94c6bf7838324f399377c
ElieltonRamos <elieltonramos14@gmail.com>
2023-07-19 00:12:07 -0300

    update components header e wallet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,31 @@
+import { useSelector } from 'react-redux';
+import stylesLogin from '../styles/login.module.css';
+import styles from '../styles/header.module.css';
+import { RootState } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const { email } = useSelector((state: RootState) => state.user);
+  const expenses = 0;
+  return (
+    <header className={ styles.container }>
+      <div>
+        <img className={ stylesLogin.emojiLogo } src="/imgs/emojiLogo.png" alt="emoji" />
+        <img className={ stylesLogin.logo } src="/imgs/logo.svg" alt="logo" />
+        <img className={ stylesLogin.logo } src="/imgs/logo01.svg" alt="Logo" />
+      </div>
+      <section>
+        <img src="/imgs/expensesIcon.svg" alt="expenses icon" />
+        <h3 data-testid="total-field">
+          {`Total de despesas: ${expenses} `}
+          <span data-testid="header-currency-field">BRL</span>
+        </h3>
+      </section>
+      <section>
+        <img src="/imgs/perfil.svg" alt="Logo perfil" />
+        <h3 data-testid="email-field">{ email }</h3>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,8 @@ export type ActionUser = {
   type: string;
   payload: { email: string };
 };
+
+export type RootState = {
+  user: { email: string };
+  wallet: { currencies: [] };
+};
