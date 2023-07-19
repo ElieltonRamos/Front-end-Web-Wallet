d86079ff21bfaa31bc136490f19cf0ea41441453
ElieltonRamos <elieltonramos14@gmail.com>
2023-07-19 01:00:01 -0300

    criando form de cadastro de gastos

--- src/components/WalletForm.tsx
+++ src/components/WalletForm.tsx
@@ -1,5 +1,24 @@
 function WalletForm() {
-  return <div>WalletForm</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="description">Descrição da despesa</label>
+        <input type="text" id="description" data-testid="description-input" />
+        <label htmlFor="category">Categoria da despesa</label>
+        <input type="text" id="category" />
+      </div>
+      <div>
+        <label htmlFor="value">Valor</label>
+        <input type="number" id="value" data-testid="value-input" />
+        <label htmlFor="methody">Método de pagamento</label>
+        <input type="text" id="methody" />
+        <label htmlFor="moeda">Moeda</label>
+        <input type="" id="moeda" />
+        { /* parei aqui falta colocar <select> */ }
+      </div>
+      <button type="submit">Adicionar despesa</button>
+    </form>
+  );
 }
 
 export default WalletForm;
